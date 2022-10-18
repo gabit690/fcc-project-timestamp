b9dccdb52b5b824d98bcf24d501a0ea4c887d22e
gabit <gabit690@gmail.com>
2022-10-18 00:05:22 -0300

    fix index file

--- index.js
+++ index.js
@@ -11,18 +11,13 @@ let cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
 app.get("/api/", (req, res) => {
   let dateInput = new Date();
